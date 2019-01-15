3f819a2b133a7c375d32f8df9bd80a850270bdb6
hima <himacp2396@gmail.com>
2019-01-15 23:48:19 +0530

    aware interface

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,8 +1,22 @@
 package com.stackroute.domain;
 
-public class Movie {
-    public Actor actor;
-    public Movie(){}
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
+
+    public Movie()
+    {
+
+    }
+
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
 
     public Actor getActor() {
         return actor;
@@ -11,5 +25,21 @@ public class Movie {
     public void setActor(Actor actor) {
         this.actor = actor;
     }
-}
 
+    private Actor actor;
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+        Actor actor= (Actor) beanFactory.getBean("actorA");
+        System.out.println("From BeanFactoryAware : "+actor);
+    }
+
+    public void setBeanName(String input) {
+        System.out.println("BeanName : "+input);
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        Actor actor= (Actor) applicationContext.getBean("actorA");
+        System.out.println("From ApplicationContextAware : "+actor);
+    }
+}
